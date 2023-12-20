22d8c23f55edd795351868b564b1cab22a92b4a2
E-moL0587 <kai.pika.025@outlook.com>
2023-12-20 17:14:57 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
-import Int from './Int.jsx';
+import Draggable from 'react-draggable';
+import Int from './Int';
 
 const App = () => {
   const [array, setArray] = useState([]);
+  const [code, setCode] = useState([]);
 
-  const handleButtonClick = (char) => {
-    setArray((prevArray) => [...prevArray, char]);
+  const onAdd = (item) => {
+    setCode([...code, item]);
   };
 
   return (
     <div>
-      <button onClick={() => handleButtonClick('int x = 0;')}>変数(int)</button>
-      <button onClick={() => handleButtonClick('std::cin >> x;')}>入力(cin)</button>
-      <button onClick={() => handleButtonClick('std::cout << x;')}>出力(cout)</button>
+      <button onClick={() => setArray([...array, <Int key={array.length} onAdd={onAdd} />])}>変数(int)</button>
+      <button onClick={() => setArray([...array, <Cout key={array.length} onAdd={onAdd} />])}>出力(cout)</button>
 
-      <Int />
+      <div>
+        <h2>VScode</h2>
+        {code.map((item, index) => (
+          <div key={index}>
+            {`int ${item.val} = ${item.num};`}
+          </div>
+        ))}
+      </div>
 
-      {array.map((item, index) => (<div key={index}>{item}</div>))}
+      {array.map((item) => (
+        <Draggable key={item.key}>
+          <div>{item}</div>
+        </Draggable>
+      ))}
+
     </div>
   );
 };
