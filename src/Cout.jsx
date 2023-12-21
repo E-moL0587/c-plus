cd5b8e9facbc10aa8eb97836cdac4cd0a240a1df
E-moL0587 <kai.pika.025@outlook.com>
2023-12-21 19:48:57 +0900

    update

--- src/Cout.jsx
+++ src/Cout.jsx
@@ -7,12 +7,25 @@ function Cout({ onAdd }) {
     onAdd(["cout", val], val);
   }, [val, onAdd]);
 
+  const editorStyle = {
+    fontFamily: 'Consolas, monospace',
+    backgroundColor: '#1e1e1e',
+    color: '#d4d4d4',
+    borderRadius: '20px',
+  };
+
+  const codeStyle = {
+    fontSize: '14px',
+    lineHeight: '1.6',
+    margin: '20px',
+  };
+
   return (
-    <div>
-      <p>
+    <div style={editorStyle}>
+      <h5 style={codeStyle}>
         変数<input type="text" value={val} onChange={(e) => setVal(e.target.value)} size="1" />
         を出力する
-      </p>
+      </h5>
     </div>
   );
 }
--- src/Int.jsx
+++ src/Int.jsx
@@ -8,13 +8,26 @@ function Int({ onAdd }) {
     onAdd(["int", val, num], val);
   }, [val, num, onAdd]);
 
+  const editorStyle = {
+    fontFamily: 'Consolas, monospace',
+    backgroundColor: '#1e1e1e',
+    color: '#d4d4d4',
+    borderRadius: '20px',
+  };
+
+  const codeStyle = {
+    fontSize: '14px',
+    lineHeight: '1.6',
+    margin: '20px',
+  };
+
   return (
-    <div>
-      <p>
+    <div style={editorStyle}>
+      <h5 style={codeStyle}>
         変数<input type="text" value={val} onChange={(e) => setVal(e.target.value)} size="1" />
         に<input type="text" value={num} onChange={(e) => setNum(e.target.value)} size="1" />
         を加える
-      </p>
+      </h5>
     </div>
   );
 }
