d062f268fbb70310daff3ff624b94a8e270dd7b9
E-moL0587 <kai.pika.025@outlook.com>
2023-12-22 20:20:54 +0900

    update

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -16,30 +16,47 @@ const Terminal = ({ sortedCode }) => {
     margin: '20px',
   };
 
+  const renderCodeItem = (item, index, a) => {
+    if (item[0] === "int" && /^-?\d+$/.test(item[1])) { // intかつ数値
+      return (
+        <div key={index}>
+          エラー: 変数 {item[1]} が 数値 です！<br />
+        </div>
+      );
+    }
+    if (item[0] === "cout") { // 出力が宣言されてる場合
+      const val = sortedCode.filter((c) => c[0] === "int" && c[1] === a); // それがいくつあるか
+
+      if (val.length === 1) { // もしひとつあったら
+        return renderCodeItem(item, index, val[0][2]); // 再帰する
+      } else if (val.length > 1) { // もし複数定義されてたら
+        return (
+          <div key={index}>
+            エラー: 変数 {a} が 複数 定義 されています！<br />
+          </div>
+        );
+      } else if (/^-?\d+$/.test(a)) { // なかったら
+        return (
+          <div key={index}>
+            {a}
+          </div>
+        );
+      } else {
+        return (
+          <div key={index}>
+            エラー: 変数 {a} が 定義 されてません！<br />
+          </div>
+        );
+      }
+    }
+    return null;
+  };
+
   return (
     <div style={editorStyle}>
       <h2 style={{ color: '#569cd6' }}>Terminal</h2>
       <div style={codeStyle}>
-        {sortedCode.map((item, index) => {
-          if (item[0] === "cout") {
-            const val = sortedCode.filter((c) => c[0] === "int" && c[1] === item[1]);
-
-            if (val.length === 1) {
-              return (
-                <div key={index}>
-                  {val[0][2]}
-                </div>
-              );
-            } else if (val.length > 1) {
-              return (
-                <div key={index}>
-                  エラー: {item[1]} が複数、定義されています！<br />
-                </div>
-              );
-            }
-          }
-          return null;
-        })}
+        {sortedCode.map((item, index) => renderCodeItem(item, index, item[1]))}
       </div>
     </div>
   );
