35df25082da874d79dc9f28062f9b20b4357c111
E-moL0587 <kai.pika.025@outlook.com>
2023-12-28 23:11:18 +0900

    update

--- src/0_App.jsx
+++ src/0_App.jsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
-import Control  from './1-1_Control';
+import Control from './1-1_Control';
 import CodeItem from './1-2_CodeItem';
-import Editor   from './2_Editor';
+import Editor from './2_Editor';
 import Terminal from './3_Terminal';
 
 const App = () => {
   const [array, setArray] = useState([]);
   const [code, setCode] = useState([]);
+  const [h, setH] = useState([]);
 
-  const updateArray = (array) => { setArray(array); };
-  const updateCode = (code) => { setCode(code); };
+  const updateArray = (array) => {
+    setArray(array);
+  };
+
+  const updateCode = (code) => {
+    setCode(code);
+  };
 
   const onDrag = (ui, key) => {
     const WindowY = (ui.y / window.innerHeight) * 100;
 
-    setCode((prevCode) => {
-      const newCode = [...prevCode];
-      newCode[key] = { ...newCode[key], y: WindowY };
-      return newCode;
+    setH((prevH) => {
+      const newH = [...prevH];
+      newH[key] = WindowY;
+      return newH;
     });
   };
 
-  const sortedCode = [...code].sort((a, b) => a.y - b.y);
+  const sortedCode = code.map((item, index) => ({ ...item, y: h[index] }));
 
   return (
     <div style={{ display: 'flex' }}>
@@ -29,12 +35,12 @@ const App = () => {
         <Control array={array} updateArray={updateArray} updateCode={updateCode} />
 
         {array.map((item, index) => (
-          <CodeItem key={index} item={item} index={index} onDrag={onDrag} />
+          <CodeItem key={index} item={item} index={index} onDrag={(ui) => onDrag(ui, index)} />
         ))}
       </div>
 
       <div style={{ zIndex: 1, flex: '1', position: 'relative' }}>
-        <Editor sortedCode={sortedCode} />
+        <Editor sortedCode={sortedCode.sort((a, b) => a.y - b.y)} />
         <Terminal sortedCode={sortedCode} />
       </div>
     </div>
